3c1d4c39229996518131d5324973fc44aacf4ec8
quhuainan <1026323214@qq.com>
2017-09-23 12:07:04 +0800

    完成数据与UI的交互

--- index.android.js
+++ index.android.js
@@ -16,20 +16,63 @@ import {
 
 let widthOfMargin = Dimensions.get('window').width * 0.05;
 export default class HelloWorld extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputedNum: '',
+            inputedPW: '',
+
+        };
+        this.updatePW = this.updatePW.bind(this);
+    }
+
+    shouldComponentUpdate() {
+        if (this.state.inputedNum.length < 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    updateNum(newText) {
+        this.setState((state) => {
+            for (var aName in state) {
+                console.log(aName);
+                console.log(state[aName])
+            }
+            return {
+                inputedNum: newText,
+                aBrandnewStateVariable: 'I am a new variable.'
+            };
+        }, this.changeNumDone);
+    }
+
+    changeNumDone() {
+        console.log('React Native has changed inputed Num')
+    }
+
+    updatePW(newText) {
+        this.setState((state) => {
+            return {inputedPW: newText};
+        });
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <TextInput style={styles.textInputStyle}
-                           placeholder={'请输入手机号'}/>
+                           placeholder={'请输入手机号'}
+                           onChangeText={(newText) => this.updateNum(newText)}/>
 
                 <Text style={styles.textPromptStyle}>
-                    您输入的手机号：
+                    您输入的手机号：{this.state.inputedNum}
                 </Text>
                 <TextInput style={styles.textInputStyle}
-                placeholder={'请输入密码'}
-                secureTextEntry={true}/>
-                <Text style={styles.bigTextPrompt} >
-                    确   定
+                           placeholder={'请输入密码'}
+                           onChangeText={this.updatePW}
+                           secureTextEntry={true}/>
+                <Text style={styles.bigTextPrompt}>
+                    确 定
                 </Text>
 
             </View>
@@ -48,15 +91,15 @@ const styles = StyleSheet.create({
         margin: widthOfMargin,
     },
     textPromptStyle: {
-        margin:widthOfMargin,
-        fontSize:20
+        margin: widthOfMargin,
+        fontSize: 20
     },
-    bigTextPrompt:{
-        margin:widthOfMargin,
-        backgroundColor:'gray',
-        color:'white',
-        textAlign:'center',
-        fontSize:30
+    bigTextPrompt: {
+        margin: widthOfMargin,
+        backgroundColor: 'gray',
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 30
     }
 });
 
