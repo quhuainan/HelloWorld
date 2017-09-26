31024b86707f5d2ee87a034c815b3c26c1f8d3fc
quhuainan <1026323214@qq.com>
2017-09-26 17:21:17 +0800

    rn Image 使用

--- index.android.js
+++ index.android.js
@@ -4,50 +4,65 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry, Image,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+let nativeSource = require('nativeImageSource');
 export default class HelloWorld extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        var imageAddress = "http://pic.58pic.com/58pic/13/76/85/69x58PICm2u_1024.jpg";
+        let ad = {android: "ic_launcher", width: 96, height: 96}
+        return (
+
+            <View style={styles.container}>
+                {/*加载native本地图片 该图片需要放在drawable 文件夹下 切需要重新编辑整个文件*/}
+                <Image style={styles.welcome}
+                       source={nativeSource(ad)}/>
+                <Image source={{uri: "ic_launcher"}}
+                       style={{width:40, height:40}}/>
+                {/*加载本地图片*/}
+                <Image style={styles.welcome}
+                 source={require('./img/ic_launcher.png')}/>
+                {/*加载网络图片*/}
+
+
+                <Image style={styles.welcome}
+                       source={{uri: imageAddress}}/>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        margin: 10,
+        borderRadius: 90,
+        width: 180,
+        height: 180,
+        borderWidth: 2,
+        borderColor: "grey"
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
